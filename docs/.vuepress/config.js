d3e48c505abe566d3b15e46adb474a4bc9c40bce
bingling-sama <65523047+bingling-sama@users.noreply.github.com>
2020-10-08 20:15:53 +0800

    bug fixed

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,14 +19,13 @@ module.exports = {
                 title: '成员名单',
                 path: '/guide/memberList',
                 children: [
-                    ['/member/binglingsama', '冰凌sama'],
-                    ['/member/beanflame', '豆焰'],
-                    ['/member/cursor', 'CursoR_光标'],
-                    ['/member/konara', '凉宫长栎'],
-                    ['/member/peachskin', '桃子皮'],
-                    ['/member/whitenight', '白夜']
+                    ['/guide/member/binglingsama', '冰凌sama'],
+                    ['/guide/member/beanflame', '豆焰'],
+                    ['/guide/member/cursor', 'CursoR_光标'],
+                    ['/guide/member/konara', '凉宫长栎'],
+                    ['/guide/member/peachskin', '桃子皮'],
+                    ['/guide/member/whitenight', '白夜']
                 ],
-                collapsable: false
             },  
             '/guide/projectList',
             '/guide/quotations'
